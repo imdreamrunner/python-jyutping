3dc587f774ee218126911b0e6fe5a3149daa248c
Ivor Zhou <ivorz@sg.ibm.com>
2017-10-23 17:40:57 +0800

    Version 0.2dev.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='jyutping',
-    version='0.1dev',
+    version='0.2dev',
     packages=['jyutping',],
     license='MIT',
     author='Ivor Zhou',
