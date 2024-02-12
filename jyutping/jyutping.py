13439112806130d9d0032e1552e0def6976bad52
Shen Zhuoran <cmsflash99@gmail.com>
2024-02-12 00:46:07 -0800

    Support multiple jyutpings for one character

--- jyutping/jyutping.py
+++ jyutping/jyutping.py
@@ -5,28 +5,30 @@ Convert Chinese characters to Jyutping.
 from __future__ import absolute_import
 from jyutping import dictionary
 
-def get_jyutping(characters):
+def get_jyutping(characters, multiple=False):
     """
     Convert Chinese characters to Jyutping.
     @return an array of Jyutping for each character.
     """
     result = []
     for ch in characters:
-        result.append(search_single(ch))
+        result.append(search(ch, multiple))
     return result
 
 
-def search_single(character):
-    if len(dictionary.CHS_DICT) == 0 or len(dictionary.CHT_DICT) == 0:
+def search(character, multiple=False):
+    if not dictionary.CHS_DICT or not dictionary.CHT_DICT:
         dictionary.load_dictionary()
-    jyp = dictionary.CHS_DICT.get(character) or dictionary.CHT_DICT.get(character)
-    if jyp and '/' in jyp:
-        jyp = jyp.split('/')
-    return jyp
+    jyupings = dictionary.CHS_DICT.get(character, set()) | dictionary.CHT_DICT.get(character, set())
+    if multiple:
+        return jyupings
+    else:
+        return jyupings.pop() if jyupings else None
 
 
 def _test(word):
     print(word, get_jyutping(word))
+    print(word, get_jyutping(word, True))
 
 if __name__ == '__main__':
     _test('广东话')
